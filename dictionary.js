9d64580859f0566851241da6a6c08be58073f80a
AlexandruFlorinB <101994570+AlexandruFlorinB@users.noreply.github.com>
2022-03-27 17:45:53 +0300

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,14 +1,14 @@
-let arrayWord = ["\0"]
+let wordArray = ["\0"]
 function isWord() {
     let word = document.getElementById("word_box").value
-    for (let index = 0; index < arrayWord.length; ++index) {
-        if (arrayWord[index] === word && (word)) {
+    for (let index = 0; index < wordArray.length; ++index) {
+        if (wordArray[index] === word && (word)) {
             document.getElementById("search").value = "The word is in Dictionary"
-            index = arrayWord.length
-        } else if (index === arrayWord.length - 1) {
+            index = wordArray.length
+        } else if (index === wordArray.length - 1) {
             document.getElementById("search").value = "The word is not in Dictionary"
         }
     }
-    arrayWord.push(word)
+    wordArray.push(word)
     document.getElementById("word_box").value = ""
 }
